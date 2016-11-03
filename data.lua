1aeb8cfa568bb07cfb8f8c9eeb1ec538a288ba78
bigyihsuan <bigyihsuan@gmail.com>
2016-11-03 14:53:49 -0400

    Initial Commit. Also finished.

---
+++ data.lua
@@ -0,0 +1,14 @@
+data:extend({
+	{
+		type = "recipe",
+		name = "wood",
+		category = "electronics",
+		enabled = true,
+		ingredients =
+		{
+		  {"raw-wood", 1},
+		},
+		result = "wood",
+		result_count = 2
+	}
+})
