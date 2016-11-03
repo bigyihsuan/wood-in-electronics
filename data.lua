62f234dcc49bc1915f66e9b63283300d87c8693e
bigyihsuan <bigyihsuan@gmail.com>
2016-11-03 17:15:21 -0400

    add resin

--- data.lua
+++ data.lua
@@ -10,5 +10,17 @@ data:extend({
 		},
 		result = "wood",
 		result_count = 2
+	},
+	{
+		type = "recipe",
+		name = "resin",
+		category = "electronics",
+		enabled = true,
+		ingredients =
+		{
+		  {"raw-wood", 1},
+		},
+		result = "resin",
+		result_count = 1
 	}
 })
